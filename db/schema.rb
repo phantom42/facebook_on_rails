07fb843e473ed30a90458c106ffbc6acb33dd58d
Joe Coleman <phantom42@gmail.com>
2015-04-29 23:31:17 -0400

    configured user model, added facebook credentials, modified devise initializer for omniauth,  added the abook rb file, generated migration for oauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429035010) do
+ActiveRecord::Schema.define(version: 20150430032724) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,11 @@ ActiveRecord::Schema.define(version: 20150429035010) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
